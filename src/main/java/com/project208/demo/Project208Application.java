1f7322e87061e923494f709acb59db963faeaa01
mohandwivedi01 <manmohandwivedi@yahoo.com>
2024-09-20 18:45:00 +0530

    intial commit

---
+++ src/main/java/com/project208/demo/Project208Application.java
@@ -0,0 +1,13 @@
+package com.project208.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project208Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project208Application.class, args);
+	}
+
+}
